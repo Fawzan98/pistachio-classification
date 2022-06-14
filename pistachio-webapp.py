b157202af2d6f208e86e586a40445dba958111bd
Fawzan Mahyuddin <58148966+Fawzan98@users.noreply.github.com>
2022-06-14 21:58:23 +0800

    Update pistachio-webapp.py

--- pistachio-webapp.py
+++ pistachio-webapp.py
@@ -10,11 +10,15 @@ import streamlit as st
 
 st.write("""
 # Pistachio Classification Prediction App
-This app capable on making **classification** on Pistachio by feeding the various types of Pistachio into a deep learning model named VGG116.\n
+This app capable on making **classification** on Pistachio images by feeding the various types of Pistachio images into a deep learning model named VGG116.\n
 VGG16 is a convolutional neural network with 16 layers deep and the model used transfer learning where the model has been pre-trained through more than a million images from ImageNet database.\n
 Coding source inspiration : [Pistachio Detection by Ayush Verma](https://www.kaggle.com/code/ayushv322/pistachio-detection-vgg16-97-9) 
 and [Step By Step VGG16 Implementation In Keras For Beginners](https://towardsdatascience.com/step-by-step-vgg16-implementation-in-keras-for-beginners-a833c686ae6c)
 """)
 
-st.title("The app title")
-st.header("Pistachio Classification Prediction App")
+st.header("Dataset")
+
+st.write("""
+The dataset obtained in Kaggle which contain a total of 2148 images, 1232 of Kirmizi and 916 of Siirt Pistachio.
+
+""")
