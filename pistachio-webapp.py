7718a859c671980fd17dd61fba23820861e51a37
Fawzan Mahyuddin <58148966+Fawzan98@users.noreply.github.com>
2022-06-14 22:54:23 +0800

    Update pistachio-webapp.py

--- pistachio-webapp.py
+++ pistachio-webapp.py
@@ -34,7 +34,7 @@ st.write(os.listdir())
 #extract dataset from rar into file
 
 
-patoolib.extract("pistachio_imgdataset.rar", outdir="/content")
+patoolib.extract_archive("pistachio_imgdataset.rar", outdir="/content")
 
 splitfolders.ratio("./content/Pistachio_Image_Dataset", output="output", seed=239, ratio=(.8, .2))
 
