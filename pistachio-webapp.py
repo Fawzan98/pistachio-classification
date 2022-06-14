c01a8fe6f3bce389a104ace4535de1980af809c5
Fawzan Mahyuddin <58148966+Fawzan98@users.noreply.github.com>
2022-06-14 21:38:06 +0800

    Update pistachio-webapp.py

--- pistachio-webapp.py
+++ pistachio-webapp.py
@@ -11,7 +11,9 @@ import streamlit as st
 st.write("""
 # Pistachio Classification Prediction App
 This app will predicts the **Pistachio** based on its type by using VGG16, a convolutional neural network with 16 layers deep!\n
-Furthermore, the model used transfer learning where the model has been pre-trained from ImageNet database.
+Furthermore, the model used transfer learning where the model has been pre-trained from ImageNet database.\n
+Coding source inspiration : [Pistachio detection by Ayush Verma](https://www.kaggle.com/code/ayushv322/pistachio-detection-vgg16-97-9) 
+and [Step by step VGG16 implementation in Keras for beginners](https://towardsdatascience.com/step-by-step-vgg16-implementation-in-keras-for-beginners-a833c686ae6c)
 """)
 
 st.title("The app title")
