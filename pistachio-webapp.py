e3aaa7657260e2ac4174fa020fb6c28627d41ed0
Fawzan Mahyuddin <58148966+Fawzan98@users.noreply.github.com>
2022-06-15 22:37:36 +0800

    Update pistachio-webapp.py

--- pistachio-webapp.py
+++ pistachio-webapp.py
@@ -46,7 +46,7 @@ st.write("Current working directory: {0}".format(cwd))
 #for subfolder_name in labels:
 #  os.makedirs(os.path.join('./images', subfolder_name))
 
-os.chdir(r"images")
+os.chdir("/app/pistachio-classification/images")
 st.write(os.listdir())
 i = 1
 
