82519d73d0105f184ea3678439e8982d0520be46
Fawzan Mahyuddin <58148966+Fawzan98@users.noreply.github.com>
2022-06-15 12:02:53 +0800

    Update pistachio-webapp.py

--- pistachio-webapp.py
+++ pistachio-webapp.py
@@ -32,11 +32,11 @@ data_url = "https://github.com/Fawzan98/pistachio-classification/tree/main/img_t
 
 os.mkdir("images")
 i = 1
-while i <= 10:
-  wget.download(data_url + f"Kirmizi/kirmizi ({i}).jpg", out = "images")
-  wget.download(data_url + f"Siirt/siirt ({i}).jpg", out = "images")
+#while i <= 10:
+#  wget.download(data_url + f"Kirmizi/kirmizi ({i}).jpg", out = "images")
+ # wget.download(data_url + f"Siirt/siirt ({i}).jpg", out = "images")
   i = i + 1
-
+os.chdir("images")
 #os.remove("pistachio_imgdataset (1).rar")
 st.write(os.listdir())
 
