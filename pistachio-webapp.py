5b48a8be67f1901f3dce1f19e597c123c8a1ef02
Fawzan Mahyuddin <58148966+Fawzan98@users.noreply.github.com>
2022-06-15 13:36:27 +0800

    Update pistachio-webapp.py

--- pistachio-webapp.py
+++ pistachio-webapp.py
@@ -30,7 +30,7 @@ The datasets then split into 80% for training and 20% for testing phase.
 """)
 
 
-data_url = "https://github.com/Fawzan98/pistachio-classification/tree/main/img_testing/"
+#data_url = "https://github.com/Fawzan98/pistachio-classification/tree/main/img_testing/"
 
 #os.mkdir("images")
 #i = 1
@@ -38,7 +38,7 @@ data_url = "https://github.com/Fawzan98/pistachio-classification/tree/main/img_t
 #  wget.download(data_url + f"Kirmizi/kirmizi ({i}).jpg", out = "images")
  # wget.download(data_url + f"Siirt/siirt ({i}).jpg", out = "images")
  # i = i + 1
-os.chdir("content/model")
+os.chdir("../..")
 #os.remove("pistachio_imgdataset (1).rar")
 #os.chdir("..")
 
