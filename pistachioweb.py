46fe4552b2d5636177021a937ef21904d67f2ddc
Fawzan Mahyuddin <58148966+Fawzan98@users.noreply.github.com>
2022-06-16 13:07:45 +0800

    Update pistachioweb.py

--- pistachioweb.py
+++ pistachioweb.py
@@ -6,6 +6,7 @@ st.sidebar.markdown('''
 # Table of Contents
 - [Introduction](#introduction)
 - [Dataset](#dataset)
+- [Data Preprocessing](#preprocessing)
 
 ''', unsafe_allow_html=True)
 
@@ -29,10 +30,15 @@ The datasets were obtained in Kaggle which contain a total of 2148 images, 1232 
 ''')
 
 img2 = ['kirmizi (23).jpg', 'siirt 24.jpg']
-st.image(img2,width=300, caption=["Kirmizi Pistachio", "Siirt Pistachio"], )
+st.image(img2,width=300, caption=["Kirmizi Pistachio", "Siirt Pistachio"])
 
+st.write('''
+We can see that both Pistachios' look very similar though some of them has distinct features though it can be difficult to recognize from naked eye.
+Now we will be using an Artificial Intelligence to reach conclusion where it could make prediction between Kirmizi and Siirt Pistachio.
+''')
 
-
+st.header('Data Preprocessing')
+st.subheader('Data Augmentation')
 st.write('''
 Before splitting the image dataset into 80% training and 20% testing, the datasets undergoes data augmentation 
 The datasets then split into 80% for training and 20% for testing phase.
