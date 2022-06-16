d342d9c2a6c212075f3b6f805137b4216012253a
Fawzan Mahyuddin <58148966+Fawzan98@users.noreply.github.com>
2022-06-16 12:40:31 +0800

    Update pistachioweb.py

--- pistachioweb.py
+++ pistachioweb.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.sidebar.markdown('''
 # Table of Contents
-- [Introduction](#intorduction)
+- [Introduction](#introduction)
 - [Dataset](#dataset)
 
 ''', unsafe_allow_html=True)
@@ -26,6 +26,17 @@ st.image(img1, caption = "VGG16 network architecture", use_column_width='always'
 st.header('Dataset')
 st.write('''
 The datasets were obtained in Kaggle which contain a total of 2148 images, 1232 of Kirmizi and 916 of Siirt Pistachio.\n
+''')
+
+
+img2 = Image.open('kirmizi (23).jpg')
+st.image(img2, caption = "Kirmizi Pistachio", use_column_width='always')
+
+img3 = Image.open('siirt 24.jpg')
+st.image(img2, caption = "Siirt Pistachio", use_column_width='always')
+
+
+st.write('''
 Before splitting the image dataset into 80% training and 20% testing, the datasets undergoes data augmentation 
 The datasets then split into 80% for training and 20% for testing phase.
 ''')
