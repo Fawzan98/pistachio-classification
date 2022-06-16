5c44b79777e5b1e2a2d192f3b3fb71dae996426e
Fawzan Mahyuddin <58148966+Fawzan98@users.noreply.github.com>
2022-06-16 14:33:54 +0800

    Update pistachioweb.py

--- pistachioweb.py
+++ pistachioweb.py
@@ -6,7 +6,7 @@ st.sidebar.markdown('''
 # Table of Contents
 - [Introduction](#introduction)
 - [Dataset](#dataset)
-- [Data Preprocessing](#preprocessing)
+- [Data Preprocessing](#data preprocessing)
 
 ''', unsafe_allow_html=True)
 
@@ -34,11 +34,17 @@ st.image(img2,width=300, caption=["Kirmizi Pistachio", "Siirt Pistachio"])
 
 st.write('''
 We can see that both Pistachios' look very similar though some of them has distinct features though it can be difficult to recognize from naked eye.
-Now we will be using an Artificial Intelligence to reach conclusion where it could make prediction between Kirmizi and Siirt Pistachio.
+Now we will be using an Artificial Intelligence to reach conclusion where it could learn to make predictions between Kirmizi and Siirt Pistachio.
 ''')
 
 st.header('Data Preprocessing')
-st.subheader('Data Augmentation')
+st.subheader('Image Augmentation')
+
+st.write('''
+A a technique of applying different transformations to original images which results in multiple transformed copies of the same image.
+This technique not only expand the size of your dataset but also incorporate a level of variation in the dataset which allows your model to generalize better on unseen data. 
+Also, the model becomes more robust when it is trained on new, slightly altered images.
+''')
 st.write('''
 Before splitting the image dataset into 80% training and 20% testing, the datasets undergoes data augmentation 
 The datasets then split into 80% for training and 20% for testing phase.
